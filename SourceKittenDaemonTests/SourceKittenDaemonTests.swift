cb33e9f4241323816dc4c7060148cc219acbaaaa
Benedikt Terhechte <terhechte@gmail.com>
2015-11-15 01:25:04 +0100

    Wrote simple server tests

--- SourceKittenDaemonTests/SourceKittenDaemonTests.swift
+++ SourceKittenDaemonTests/SourceKittenDaemonTests.swift
@@ -7,29 +7,39 @@
 //
 
 import XCTest
+@testable import SourceKittenDaemon
 
-class SourceKittenDaemonTests: XCTestCase {
+extension Completer {
+    func complete(filePath: String, fileInProject: String, offset: Int) -> CompletionResult {
+        return CompletionResult.Failure(message: "Done")
+    }
+}
+
+class SourceKittenDaemonServerTests: XCTestCase {
+    
+    var server: CompletionServer? = nil
+    let port = 9981
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let project = ProjectType.Folder(path: "")
+        let completer = Completer(project: project)
+        
+        // FIXME: What happens if the port is blocked? Continue until we find a working port?
+        self.server = SourceKittenDaemon.CompletionServer(completer: completer, port: self.port)
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        self.server?.stop()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testResponse() {
+        let request = NSURLRequest(URL: NSURL(string: "http://localhost:\(self.port)/complete")!)
+        let result = try? NSURLConnection.sendSynchronousRequest(request, returningResponse: nil)
+        XCTAssertNotNil(result, "Has to get a proper result")
+        let responseString = String(data: result!, encoding: NSUTF8StringEncoding)
+        XCTAssertTrue(responseString == "{\"error\": \"Done\"}")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }

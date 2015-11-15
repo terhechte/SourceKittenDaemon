70bb70792861df91596ac1dff735e7e63ccc439f
Benedikt Terhechte <terhechte@gmail.com>
2015-11-15 14:56:28 +0100

    Added preliminary, working, server unit tests. the stop server test still fails, looking into that

--- SourceKittenDaemon/main.swift
+++ SourceKittenDaemon/main.swift
@@ -6,10 +6,11 @@
 //  Copyright © 2015 Benedikt Terhechte. All rights reserved.
 //
 
-import Foundation
+import Cocoa
 import Commandant
+import Result
 
-enum SourceKittenDaemonError: CustomStringConvertible {
+internal enum SourceKittenDaemonError: CustomStringConvertible {
     /// One or more argument was invalid.
     case InvalidArgument(description: String)
     
@@ -27,11 +28,42 @@ enum SourceKittenDaemonError: CustomStringConvertible {
     }
 }
 
+@objc class TimerDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(notification: NSNotification) {
+        
+    }
+}
+
+/**
+This is currently the only way I could think of to allow unit tests.
+This command will run the app like a cocoa app, so that unit tests
+can be run.
+There may be a different way with arcane unit test xcode knowledge, but cursory
+ googling did not yield anything worthwhile.
+*/
+struct ServerTestCommand: CommandType {
+    let verb = "test"
+    let function = "mock command for running the test suite"
+    
+    func run(mode: CommandMode) -> Result<(), CommandantError<SourceKittenDaemonError>> {
+        let delegate = TimerDelegate()
+        
+        let app = NSApplication.sharedApplication()
+        app.delegate = delegate
+        app.run()
+        
+        return .Success(())
+    }
+}
+
 let registry = CommandRegistry<SourceKittenDaemonError>()
 registry.register(ServerStartCommand())
 
+registry.register(ServerTestCommand())
+
 let helpCommand = HelpCommand(registry: registry)
 registry.register(helpCommand)
+
 
 registry.main(defaultVerb: "help") { error in
     fputs("\(error)\n", stderr)
--- SourceKittenDaemonTests/SourceKittenDaemonTests.swift
+++ SourceKittenDaemonTests/SourceKittenDaemonTests.swift
@@ -9,11 +9,13 @@
 import XCTest
 @testable import SourceKittenDaemon
 
+
+
 // Tests still need fixtures.
 class SourceKittenDaemonTests: XCTestCase {
     
     var server: CompletionServer? = nil
-    let port = 9981
+    let port = 9982
     
     override func setUp() {
         super.setUp()
@@ -21,20 +23,43 @@ class SourceKittenDaemonTests: XCTestCase {
         let completer = Completer(project: project)
         
         // FIXME: What happens if the port is blocked? Continue until we find a working port?
-        self.server = SourceKittenDaemon.CompletionServer(completer: completer, port: self.port)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) { () -> Void in
+            self.server = SourceKittenDaemon.CompletionServer(completer: completer, port: self.port)
+        }
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-        self.server?.stop()
+    }
+    
+    func serverURLWithCommand(command: String) -> NSURLRequest {
+        let request = NSURLRequest(URL: NSURL(string: "http://localhost:\(self.port)/\(command)")!)
+        return request
     }
     
     func testResponse() {
-        let request = NSURLRequest(URL: NSURL(string: "http://localhost:\(self.port)/complete")!)
+        let request = self.serverURLWithCommand("complete")
         let result = try? NSURLConnection.sendSynchronousRequest(request, returningResponse: nil)
         XCTAssertNotNil(result, "Has to get a proper result")
-        let responseString = String(data: result!, encoding: NSUTF8StringEncoding)
-        XCTAssertTrue(responseString == "{\"error\": \"Done\"}")
+        let jsonResponse = try? NSJSONSerialization.JSONObjectWithData(result!, options: [])
+        XCTAssertNotNil(jsonResponse, "Expected proper json response")
+        XCTAssertTrue((jsonResponse?.isKindOfClass(NSDictionary.classForCoder())) != nil, "Expected dictionary")
+        let errorMessage = (jsonResponse! as? NSDictionary)!["error"]
+        XCTAssertNotNil(errorMessage, "Expected an error message")
+    }
+    
+    func testStopServer() {
+        self.server?.stop()
+        self.server = nil
+        let request = self.serverURLWithCommand("complete")
+        let expectation = expectationWithDescription("Expect the server to be offline")
+        
+        waitForExpectationsWithTimeout(5) { error in
+            let result = try? NSURLConnection.sendSynchronousRequest(request, returningResponse: nil)
+            if result == nil {
+                expectation.fulfill()
+            }
+        }
     }
 }

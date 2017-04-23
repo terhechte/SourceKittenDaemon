def64e2ccee4da427839e6e4180955806f7e1e8d
Nathan Kot <nathankot@users.noreply.github.com>
2017-04-23 15:47:38 +0900

    Upgrade sourcekitten, resolve build error in newer version of Xcode (#53)

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
   ],
 
   dependencies: [
-    .Package(url: "https://github.com/Carthage/Commandant.git", Version(0, 11, 2)),
-    .Package(url: "https://github.com/jpsim/SourceKitten.git", Version(0, 15, 0)),
+    .Package(url: "https://github.com/Carthage/Commandant.git", versions: Version(0, 12, 0)..<Version(0, 12, .max)),
+    .Package(url: "https://github.com/jpsim/SourceKitten.git", Version(0, 17, 2)),
     .Package(url: "https://github.com/vapor/vapor.git", Version(1, 1, 12)),
     .Package(url: "https://github.com/nanzhong/Xcode.swift.git", Version(0, 4, 1))
   ],

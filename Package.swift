a49a6eb3eb057d6a00c7ef42753a32079105444d
Benedikt Terhechte <terhechte@gmail.com>
2017-04-28 22:20:58 +0200

    [WIP] Cleanup Removed Vapor Dependency, +Embassy
    This is a WIP test commit to see how the project performs when replacing
    Vapor with Embassy. Embassy is much more lightweight, resulting in
    shorter compile times.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/Carthage/Commandant.git", versions: Version(0, 12, 0)..<Version(0, 12, .max)),
     .Package(url: "https://github.com/jpsim/SourceKitten.git", Version(0, 17, 2)),
-    .Package(url: "https://github.com/vapor/vapor.git", Version(1, 1, 12)),
+    .Package(url: "https://github.com/envoy/Embassy.git",
+                 majorVersion: 3),
     .Package(url: "https://github.com/nanzhong/Xcode.swift.git", Version(0, 4, 1))
   ],
 
--- Sources/SourceKittenDaemon/Server/CompletionServer.swift
+++ Sources/SourceKittenDaemon/Server/CompletionServer.swift
@@ -7,7 +7,11 @@
 //
 
 import Foundation
-import Vapor
+import Embassy
+
+enum Abort: Error {
+    case custom(status: Int, message: String)
+}
 
 /**
 This runs a simple webserver that can be queried from Emacs to get completions
@@ -15,73 +19,118 @@ for a file.
 */
 public class CompletionServer {
 
-    let port: Int
     let completer: Completer
+    let loop: SelectorEventLoop
+    
+    var server: DefaultHTTPServer?
 
-    let droplet = Droplet(arguments: ["vapor", "serve"])
-
-    public init(project: Project, port: Int) {
+    public init(project: Project, port: Int) throws {
+        let selector = try KqueueSelector()
+        self.loop = try SelectorEventLoop(selector: selector)
         self.completer = Completer(project: project)
-        self.port = port
-
-        droplet.get("/ping") { request in
-            return "OK"
-        }
-
-        droplet.get("/project") { request in
-            return self.completer.project.projectFile.path
-        }
-
-        droplet.get("/files") { request in
-            let files = self.completer.sourceFiles()
-            guard let jsonFiles = try? JSONSerialization.data(
-                    withJSONObject: files,
-                    options: JSONSerialization.WritingOptions.prettyPrinted
-                  ),
-                  let filesString = String(data: jsonFiles, encoding: String.Encoding.utf8) else {
-                throw Abort.custom(
-                  status: .badRequest,
-                  message: "{\"error\": \"Could not generate file list\"}"
-                )
-            }
-            return filesString
-        }
-
-        droplet.get("/complete") { request in
-            guard let offsetString = request.headers["X-Offset"],
-                  let offset = Int(offsetString) else {
-                throw Abort.custom(
-                  status: .badRequest,
-                  message: "{\"error\": \"Need X-Offset as completion offset for completion\"}"
-                )
-            }
-
-            guard let path = request.headers["X-Path"] else {
-                throw Abort.custom(
-                  status: .badRequest,
-                  message: "{\"error\": \"Need X-Path as path to the temporary buffer\"}"
-                )
-            }
-
-            print("[HTTP] GET /complete X-Offset:\(offset) X-Path:\(path)")
-
-            let url = URL(fileURLWithPath: path)
-            let result = self.completer.complete(url, offset: offset)
-
-            switch result {
-            case .success(result: _):
-                return result.asJSONString()!
-            case .failure(message: let msg):
-                throw Abort.custom(
-                  status: .badRequest,
-                  message: "{\"error\": \"\(msg)\"}"
-                )
-            }
-        }
+        self.server = DefaultHTTPServer(eventLoop: loop, port: port, app: requestHandler)
     }
 
-    public func start() {
-        droplet.run(servers: ["default": (host: "0.0.0.0", port: port, securityLayer: .none)])
+    public func start() throws {
+        try server?.start()
+        // Run event loop
+        loop.runForever()
+
     }
 
+    private func requestHandler(environ: [String: Any], startResponse: ((String, [(String, String)]) -> Void), sendBody: ((Data) -> Void)) {
+        do {
+            let returnResult = try routeRequest(environ: environ)
+            startResponse("200 OK", [])
+            sendBody(Data(returnResult.utf8))
+            sendBody(Data())
+        } catch Abort.custom(status: let status, message: let message) {
+            startResponse("\(status) BAD REQUEST", [])
+            sendBody(Data(message.utf8))
+            sendBody(Data())
+        } catch let error {
+            startResponse("500 INTERNAL SERVER ERROR", [])
+            sendBody(Data("\(error)".utf8))
+            sendBody(Data())
+        }
+    }
+    
+    private func routeRequest(environ: [String: Any]) throws -> String {
+        let routes = [
+            "/ping": servePing,
+            "/project": serveProject,
+            "/files": serveFiles,
+            "/complete": serveComplete,
+        ]
+        
+        guard let path = environ["PATH_INFO"] as? String,
+            let route = routes[path] else {
+                throw Abort.custom(
+                    status: 400,
+                    message: "{\"error\": \"Could not generate file list\"}"
+                )
+        }
+        
+        return try route(environ)
+    }
+}
+
+/**
+ Concrete implementation of the calls
+ */
+extension CompletionServer {
+    func servePing(environ: [String: Any]) throws -> String {
+        return "OK"
+    }
+    
+    func serveProject(environ: [String: Any]) throws -> String {
+        return self.completer.project.projectFile.path
+    }
+    
+    func serveFiles(environ: [String: Any]) throws -> String {
+        let files = self.completer.sourceFiles()
+        guard let jsonFiles = try? JSONSerialization.data(
+            withJSONObject: files,
+            options: JSONSerialization.WritingOptions.prettyPrinted
+            ),
+            let filesString = String(data: jsonFiles, encoding: String.Encoding.utf8) else {
+                throw Abort.custom(
+                    status: 400,
+                    message: "{\"error\": \"Could not generate file list\"}"
+                )
+        }
+        return filesString
+    }
+    
+    func serveComplete(environ: [String: Any]) throws -> String {
+        guard let offsetString = environ["HTTP_X_OFFSET"] as? String,
+            let offset = Int(offsetString) else {
+                throw Abort.custom(
+                    status: 400,
+                    message: "{\"error\": \"Need X-Offset as completion offset for completion\"}"
+                )
+        }
+        
+        guard let path = environ["HTTP_X_PATH"] as? String else {
+            throw Abort.custom(
+                status: 400,
+                message: "{\"error\": \"Need X-Path as path to the temporary buffer\"}"
+            )
+        }
+        
+        print("[HTTP] GET /complete X-Offset:\(offset) X-Path:\(path)")
+        
+        let url = URL(fileURLWithPath: path)
+        let result = self.completer.complete(url, offset: offset)
+        
+        switch result {
+        case .success(result: _):
+            return result.asJSONString()!
+        case .failure(message: let msg):
+            throw Abort.custom(
+                status: 400,
+                message: "{\"error\": \"\(msg)\"}"
+            )
+        }
+    }
 }
